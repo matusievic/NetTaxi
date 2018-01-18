0e21e3446b70ab1011ead1a6474d0ace80369524
Andrei Matusevich <matusievic@gmail.com>
2018-01-18 15:53:52 +0300

    Paginator service was removed

--- src/main/java/by/tc/web/service/paginator/Paginator.java
+++
@@ -1,7 +0,0 @@
-package by.tc.web.service.paginator;
-
-import by.tc.web.domain.pagination.Pagination;
-
-public interface Paginator {
-    Pagination paginate(int currentPage, int itemsPerPage);
-}
